cbcc4e9b11cdbef4c844f76287b51a085fe096f5
Madeline Watts <49796001+mw29@users.noreply.github.com>
2022-10-10 13:55:07 -0600

    Create BNN.py

---
+++ BNN.py
@@ -0,0 +1,42 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import tensorflow_datasets as tfds
+import tensorflow_probability as tfp
+
+def get_train_and_test_splits(train_size, batch_size=1):
+    dataset = (
+        tfds.load(name="wine_quality", as_supervised=True, split="train")
+        .map(lambda x, y: (x, tf.cast(y, tf.float32)))
+        .prefetch(buffer_size=dataset_size)
+        .cache()
+    )
+    train_dataset = (
+        dataset.take(train_size).shuffle(buffer_size=train_size).batch(batch_size)
+    )
+    test_dataset = dataset.skip(train_size).batch(batch_size)
+
+    return train_dataset, test_dataset
+  
+  hidden_units = [8, 8]
+learning_rate = 0.001
+
+
+def run_experiment(model, loss, train_dataset, test_dataset):
+
+    model.compile(
+        optimizer=keras.optimizers.RMSprop(learning_rate=learning_rate),
+        loss=loss,
+        metrics=[keras.metrics.RootMeanSquaredError()],
+    )
+
+    print("Start training the model...")
+    model.fit(train_dataset, epochs=num_epochs, validation_data=test_dataset)
+    print("Model training finished.")
+    _, rmse = model.evaluate(train_dataset, verbose=0)
+    print(f"Train RMSE: {round(rmse, 3)}")
+
+    print("Evaluating model performance...")
+    _, rmse = model.evaluate(test_dataset, verbose=0)
+    print(f"Test RMSE: {round(rmse, 3)}")
